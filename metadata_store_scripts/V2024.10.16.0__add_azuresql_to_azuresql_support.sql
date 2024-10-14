d236cab3f8d167fbd7cfe26c7380053a682a2620
Sumeet <sumeet.das@delphix.com>
2024-10-14 11:09:52 +0530

    feat: add support for AzureSQL to AzureSQL
    PR URL: https://www.github.com/delphix/dcs-for-azure-templates/pull/21

---
+++ metadata_store_scripts/V2024.10.16.0__add_azuresql_to_azuresql_support.sql
@@ -0,0 +1,36 @@
+DELETE FROM adf_type_mapping WHERE dataset = 'AZURESQL';
+
+INSERT INTO adf_type_mapping(dataset, dataset_type, adf_type)
+   VALUES
+('AZURESQL', 'tinyint', 'integer'),
+('AZURESQL', 'smallint', 'short'),
+('AZURESQL', 'int', 'integer'),
+('AZURESQL', 'bigint', 'long'),
+('AZURESQL', 'bit', 'boolean'),
+('AZURESQL', 'decimal', 'decimal'),
+('AZURESQL', 'numeric', 'decimal'),
+('AZURESQL', 'money', 'decimal'),
+('AZURESQL', 'smallmoney', 'decimal'),
+('AZURESQL', 'float', 'double'),
+('AZURESQL', 'real', 'float'),
+('AZURESQL', 'date', 'date'),
+('AZURESQL', 'time', 'timestamp'),
+('AZURESQL', 'datetime2', 'timestamp'),
+('AZURESQL', 'datetimeoffset', 'string'),
+('AZURESQL', 'datetime', 'timestamp'),
+('AZURESQL', 'smalldatetime', 'timestamp'),
+('AZURESQL', 'char', 'string'),
+('AZURESQL', 'varchar', 'string'),
+('AZURESQL', 'text', 'string'),
+('AZURESQL', 'nchar', 'string'),
+('AZURESQL', 'nvarchar', 'string'),
+('AZURESQL', 'ntext', 'string'),
+('AZURESQL', 'binary', 'binary'),
+('AZURESQL', 'varbinary', 'binary'),
+('AZURESQL', 'image', 'binary'),
+('AZURESQL', 'geography', 'binary'),
+('AZURESQL', 'geometry', 'binary'),
+('AZURESQL', 'json', 'string'),
+('AZURESQL', 'uniqueidentifier', 'string'),
+('AZURESQL', 'xml', 'string')
+;
--- metadata_store_scripts/bootstrap.sql
+++ metadata_store_scripts/bootstrap.sql
@@ -272,3 +272,40 @@ BEGIN
 END;
 -- source: V2024.08.25.0__add_conditional_masking_support
 ALTER TABLE discovered_ruleset ALTER COLUMN assigned_algorithm VARCHAR(MAX);
+-- source: V2024.10.16.0__add_azuresql_to_azuresql_support
+DELETE FROM adf_type_mapping WHERE dataset = 'AZURESQL';
+
+INSERT INTO adf_type_mapping(dataset, dataset_type, adf_type)
+   VALUES
+('AZURESQL', 'tinyint', 'integer'),
+('AZURESQL', 'smallint', 'short'),
+('AZURESQL', 'int', 'integer'),
+('AZURESQL', 'bigint', 'long'),
+('AZURESQL', 'bit', 'boolean'),
+('AZURESQL', 'decimal', 'decimal'),
+('AZURESQL', 'numeric', 'decimal'),
+('AZURESQL', 'money', 'decimal'),
+('AZURESQL', 'smallmoney', 'decimal'),
+('AZURESQL', 'float', 'double'),
+('AZURESQL', 'real', 'float'),
+('AZURESQL', 'date', 'date'),
+('AZURESQL', 'time', 'timestamp'),
+('AZURESQL', 'datetime2', 'timestamp'),
+('AZURESQL', 'datetimeoffset', 'string'),
+('AZURESQL', 'datetime', 'timestamp'),
+('AZURESQL', 'smalldatetime', 'timestamp'),
+('AZURESQL', 'char', 'string'),
+('AZURESQL', 'varchar', 'string'),
+('AZURESQL', 'text', 'string'),
+('AZURESQL', 'nchar', 'string'),
+('AZURESQL', 'nvarchar', 'string'),
+('AZURESQL', 'ntext', 'string'),
+('AZURESQL', 'binary', 'binary'),
+('AZURESQL', 'varbinary', 'binary'),
+('AZURESQL', 'image', 'binary'),
+('AZURESQL', 'geography', 'binary'),
+('AZURESQL', 'geometry', 'binary'),
+('AZURESQL', 'json', 'string'),
+('AZURESQL', 'uniqueidentifier', 'string'),
+('AZURESQL', 'xml', 'string')
+;
