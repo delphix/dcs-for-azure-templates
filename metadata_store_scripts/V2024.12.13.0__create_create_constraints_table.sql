cae0c98546c669df53c02bbfc817e1d6fd70883a
Sumeet <sumeet.das@delphix.com>
2024-12-10 23:10:22 +0530

    chore: added a migration script to create capture_constraints table

---
+++ metadata_store_scripts/V2024.12.13.0__create_create_constraints_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE capture_constraints (
+    dataset VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    specified_database VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    specified_schema VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    identified_parent_table VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    child_table VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    constraint_name VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    parent_columns VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    children_columns VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    before_masking_status VARCHAR(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    last_captured_updated_timestamp DATETIME,
+    after_masking_status VARCHAR(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    CONSTRAINT capture_constraints_pk PRIMARY KEY (
+        dataset, 
+        specified_database, 
+        specified_schema, 
+        identified_parent_table, 
+        child_table, 
+        constraint_name
+    )
+);
