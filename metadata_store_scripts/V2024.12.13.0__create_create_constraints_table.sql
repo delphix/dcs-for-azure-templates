c603c67d3c09162adc63cab4de7520e0ff317309
Sumeet <sumeet.das@delphix.com>
2024-12-12 01:33:57 +0530

    chore: updated create constraints table

--- metadata_store_scripts/V2024.12.13.0__create_create_constraints_table.sql
+++ metadata_store_scripts/V2024.12.13.0__create_create_constraints_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE capture_constraints (
+    pipeline_run_id UNIQUEIDENTIFIER NOT NULL,
     dataset VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     specified_database VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     specified_schema VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
@@ -9,11 +10,12 @@ CREATE TABLE capture_constraints (
     children_columns VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     pre_drop_status VARCHAR(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     drop_error_message NVARCHAR(MAX),
-    last_drop_timestamp DATETIME,
+    drop_timestamp DATETIME,
     post_create_status VARCHAR(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     create_error_message NVARCHAR(MAX),
-    last_create_timestamp DATETIME,
+    create_timestamp DATETIME,
     CONSTRAINT capture_constraints_pk PRIMARY KEY (
+        pipeline_run_id,
         dataset, 
         specified_database, 
         specified_schema, 
