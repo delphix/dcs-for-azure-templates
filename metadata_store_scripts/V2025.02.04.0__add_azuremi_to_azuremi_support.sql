49b017b078bf6903eeff30bb32f704d24ec5557c
sumeetdas-dlpx <117194297+sumeetdas-dlpx@users.noreply.github.com>
2025-02-17 12:29:40 +0530

    feat: add support for Azure MI to Azure MI (#39)
    * fix: removed join with P_METADATA_SOURCE_TO_SINK_MAPPING_TABLE in Copy Referential Constraints
    
    * updated query in "Delete Masked Table Constraints" activity
    
    * updated SQL statement in Delete Masked Table Constraints activity
    
    * updated Azure SQL documentation
    
    * updated README.md
    
    * feat: add support for Azure MI to Azure MI
    
    * chore: removed traces of Azure SQL from template
    
    * chore: fixed variable value in masking pipeline
    
    * fix: fixed values and descriptions
    
    * fix: updated adf type mappings for AzureSQL types mapped to decimal type to double
    
    * chore: updated comments
    
    * fix: changes metadata store types to Azure SQL
    
    * fix: corrected the expression in TrimMaskedStrings activity that caused the JSON, XML, and UUID masked values to be replaced by an empty string
    
    * fix: updated expression in TrimMaskedStrings in filtered_mask_df
    
    * chore: renamed pipelines
    
    * chore: fixed names
    
    * chore: revert changes in AzureSQL discovery pl README
    
    * chore: fixed names in AzureSQL MI discovery pl README
    
    * chore: fixed names related to Azure SQL MI
    
    * chore: fixed names related to Azure SQL MI in discovery pl
    
    * chore: fixed names related to Azure SQL MI
    
    * chore: added symlinks to documentation
    
    * chore: addressed review comments
    
    * chore: addressed review comments
    
    * chore: addressed review comments

---
+++ metadata_store_scripts/V2025.02.04.0__add_azuremi_to_azuremi_support.sql
@@ -0,0 +1,16 @@
+
+-- Update ADF type mappings of decimal, numeric and money to double for AzureSQL dataset
+UPDATE adf_type_mapping
+SET adf_type = 'double'
+WHERE dataset = 'AZURESQL' AND dataset_type IN ('decimal', 'numeric', 'money');
+
+-- Update ADF type mapping of smallmoney to float for AzureSQL dataset
+UPDATE adf_type_mapping
+SET adf_type = 'float'
+WHERE dataset = 'AZURESQL' AND dataset_type = 'smallmoney';
+
+-- Copy ADF type mapping from AzureSQL to AzureSQL_MI
+INSERT INTO adf_type_mapping(dataset, dataset_type, adf_type)
+SELECT 'AZURESQL-MI', dataset_type, adf_type
+FROM adf_type_mapping
+WHERE dataset = 'AZURESQL';
