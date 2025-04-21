5abfe3da4fa81f0bf4c33a94ed04bf3560cca48b
sharadpandey90 <sharad.pandey@delphix.com>
2025-04-21 21:31:47 +0530

    chore: Add pre-commit check to format sql and json files
    PR URL: https://www.github.com/delphix/dcs-for-azure-templates/pull/62

--- scripts/format_json.sh
+++ scripts/format_json.sh
@@ -10,5 +10,12 @@ do
      cat $PIPELINE/manifest.json | jq '.' > $PIPELINE/manifest.json.fmt
      mv $PIPELINE/manifest.json.fmt $PIPELINE/manifest.json
      rm $PIPELINE.zip
+  elif test -f $PIPELINE/$PIPELINE.json; then
+     cat $PIPELINE/$PIPELINE.json | jq '.' > $PIPELINE/$PIPELINE.json.fmt
+     mv $PIPELINE/$PIPELINE.json.fmt $PIPELINE/$PIPELINE.json
+     if test -f $PIPELINE/manifest.json; then
+       cat $PIPELINE/manifest.json | jq '.' > $PIPELINE/manifest.json.fmt
+       mv $PIPELINE/manifest.json.fmt $PIPELINE/manifest.json
+     fi
   fi
 done
---
+++ scripts/pre-commit/format_sql_json.py
@@ -0,0 +1,36 @@
+import subprocess
+import sys
+import helpers
+from pathlib import Path
+
+
+SCRIPT_DIR = "scripts"
+SQL_FORMATTER_SCRIPT = "format_sql.sh"
+JSON_FORMATTER_SCRIPT = "format_json.sh"
+
+
+def run_script(script_path: Path):
+    subprocess.run(
+        [script_path],
+        check=True,
+    )
+
+
+def main():
+    try:
+        sql_formatter_script = helpers.get_project_root() / SCRIPT_DIR / SQL_FORMATTER_SCRIPT
+        run_script(sql_formatter_script)
+
+        json_formatter_script = helpers.get_project_root() / SCRIPT_DIR / JSON_FORMATTER_SCRIPT
+        run_script(json_formatter_script)
+        return 0
+    except subprocess.CalledProcessError as e:
+        print(f"Error running script: {e}")
+        return 1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
