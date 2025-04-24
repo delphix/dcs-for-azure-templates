5da46d06234e8cba9e0b3eadf2f6357d18577026
Saurabh Rusia <saurabh.rusia@delphix.com>
2025-04-24 09:53:18 +0530

    chore: Add pre-commit hook to enforce naming convention
    PR URL: https://www.github.com/delphix/dcs-for-azure-templates/pull/64

--- scripts/pre-commit/helpers.py
+++ scripts/pre-commit/helpers.py
@@ -2,19 +2,36 @@
 # Copyright (c) 2025 by Delphix. All rights reserved.
 #
 
+import dataclasses
 import pathlib
+import re
 import subprocess
 import typing as tp
 
 from packaging.version import Version
 
+MANIFEST_FILE = "manifest.json"
+README_FILE = "README.md"
 VERSION_FILE = "VERSION.md"
 CHANGELOG_FILE = "CHANGELOG.md"
 TEMPLATES_JSON_PATH_PREFIX = "dcsazure_"
 METADATA_STORE_PATH = "metadata_store_scripts"
+DOCUMENTATION_PATH = "documentation"
 JSON = ".json"
 SQL = ".sql"
 MD = ".md"
+
+RESOURCE_TYPE_ABBR = {
+    "pipelines": ("P_", "_pl"),
+    "datasets": ("DS_", "_ds"),
+    "dataflows": (None, "_df"),
+}
+
+NON_TEMPLATES_DIR = [
+    DOCUMENTATION_PATH, METADATA_STORE_PATH, "releases", "scripts",
+]
+
+TEMPLATE_DIR_REGEX = r"^dcsazure_(\w+)_to_(\w+)?_(mask|discovery)_pl$"
 
 
 class GitCommand:
@@ -39,6 +56,45 @@ class VersionParsingException(Exception):
 class InvalidVersionException(Exception):
     """Exception to be raised if an incorrect version present in VERSION.md"""
     pass
+
+
+class InvalidTemplateNameException(Exception):
+    """
+    Exception to be raised if the ADF pipeline's template file or directory
+    names are not per convention.
+    """
+    pass
+
+
+class InvalidResourceNameException(Exception):
+    """
+    Exception to be raised if the resource name in the ADF pipeline's template file
+    are not per convention.
+    """
+    pass
+
+
+class InvalidParameterNameException(Exception):
+    """
+    Exception to be raised if the parameter name in the ADF pipeline's template file
+    are not per convention.
+    """
+    pass
+
+
+@dataclasses.dataclass
+class Pipeline:
+    source_db: str
+    sink_db: str
+    service: str
+
+    @classmethod
+    def from_string(cls, input_str: str):
+        match = re.match(TEMPLATE_DIR_REGEX, input_str)
+        if match:
+            return cls(match.group(1), match.group(2), match.group(3))
+        else:
+            raise ValueError("Input name can't be parsed as a pipeline name.")
 
 
 def get_cmd_output(cmd: tp.List[str]) -> str:
@@ -116,3 +172,15 @@ def get_next_version(version: Version) -> tp.List[str]:
         f"{version.major}.{version.minor + 1}.0",
         f"{version.major + 1}.0.0",
     ]
+
+
+def is_title(inp: str) -> bool:
+    """
+    Check if the input string is in title case format. E.g. Both
+    'Check CSV', 'Select Tables' will be considered as title case.
+    Python inbuilt `istitle()` returns False for the former.
+
+    :param inp: Input String
+    :return: True if input string is in title case
+    """
+    return all(substr[0].isupper() for substr in inp.strip().split())
