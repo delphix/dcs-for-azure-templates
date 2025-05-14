bb7bb096118a01111ac73b119d35cef8c3839417
sharadpandey90 <sharad.pandey@delphix.com>
2025-05-14 21:11:41 +0530

    Include sql formatting and json formatting logic in python script

--- scripts/pre-commit/format_sql_json.py
+++ scripts/pre-commit/format_sql_json.py
@@ -6,11 +6,13 @@ import subprocess
 import sys
 import helpers
 from pathlib import Path
+import typing as tp
+import logging
 
+DIALECT = "tsql"
 
-SCRIPT_DIR = "scripts/pre-commit"
-SQL_FORMATTER_SCRIPT = "format_sql.sh"
-JSON_FORMATTER_SCRIPT = "format_json.sh"
+logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
+logger = logging.getLogger("format_sql_json")
 
 
 def run_script(script_path: Path):
@@ -20,19 +22,51 @@ def run_script(script_path: Path):
     )
 
 
+def format_sql(modified_sql_files: tp.List[Path]):
+    for sql_file in modified_sql_files:
+        try:
+            subprocess.run(
+                ["sqlfluff", "format", str(sql_file), "--dialect", DIALECT],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error formatting SQL file {sql_file}: {e}")
+            raise
+
+
+def format_json(modified_json_files: tp.List[Path]):
+    for json_file in modified_json_files:
+        json_path = helpers.get_project_root() / json_file
+        temp_json_path = json_path.with_suffix(json_path.suffix + ".fmt")
+        try:
+            with temp_json_path.open("w") as temp_file:
+                subprocess.run(
+                    ["jq", ".", str(json_file)],
+                    stdout=temp_file,
+                    stderr=subprocess.DEVNULL,
+                    check=True,
+                )
+            temp_json_path.replace(json_path)
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error formatting JSON file {json_file}: {e}")
+            raise
+
+
 def main():
     try:
-        sql_formatter_script = helpers.get_project_root() / SCRIPT_DIR / SQL_FORMATTER_SCRIPT
-        run_script(sql_formatter_script)
-
-        json_formatter_script = helpers.get_project_root() / SCRIPT_DIR / JSON_FORMATTER_SCRIPT
-        run_script(json_formatter_script)
+        changed_files = helpers.get_all_modified_files()
+        modified_sql_files = [sql_file for sql_file in changed_files if sql_file.suffix == helpers.SQL]
+        modified_json_files = [json_file for json_file in changed_files if json_file.suffix == helpers.JSON]
+        format_sql(modified_sql_files)
+        format_json(modified_json_files)
         return 0
     except subprocess.CalledProcessError as e:
-        print(f"Error running script: {e}")
+        logger.error(f"Error executing command: {e}")
         return 1
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
         return 1
 
 
