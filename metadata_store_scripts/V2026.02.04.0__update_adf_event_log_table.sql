52cd10b991a83177a3df4641ff0fdfce380bb1b8
sharadpandey90 <sharad.pandey@delphix.com>
2026-02-12 13:23:22 +0530

    Address review comment of Jessica

--- metadata_store_scripts/V2026.02.04.0__update_adf_event_log_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE adf_events_log
-ALTER COLUMN source_schema VARCHAR(255) COLLATE sql_latin1_general_cp1_ci_as NULL;
-
-ALTER TABLE adf_events_log
-ALTER COLUMN sink_schema VARCHAR(255) COLLATE sql_latin1_general_cp1_ci_as NULL;
