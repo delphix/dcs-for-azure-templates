0068af8fe0d1157dfb54d89b2d7446b4bbd61087
udhay-creator <udayakumar_a@levelshift.com>
2026-03-16 16:34:52 +0530

    feat: add support for Cassandra to Cassandra

--- metadata_store_scripts/V2026.03.13.0__add_cassandra_adf_type_mapping.sql
+++ metadata_store_scripts/V2026.03.13.0__add_cassandra_adf_type_mapping.sql
@@ -1,5 +1,12 @@
 -- Insert type mapping
-INSERT INTO adf_type_mapping
-(dataset, dataset_type, adf_type)
-VALUES
-('CASSANDRA', 'String', 'string');
+INSERT INTO adf_type_mapping (
+    dataset,
+    dataset_type,
+    adf_type
+)
+VALUES (
+    'CASSANDRA',
+    'String',
+    'string'
+);
+GO
--- metadata_store_scripts/bootstrap.sql
+++ metadata_store_scripts/bootstrap.sql
@@ -4539,7 +4539,14 @@ GO
 
 -- source: V2026.03.13.0__add_cassandra_adf_type_mapping
 -- Insert type mapping
-INSERT INTO adf_type_mapping
-(dataset, dataset_type, adf_type)
-VALUES
-('CASSANDRA', 'String', 'string');
+INSERT INTO adf_type_mapping (
+    dataset,
+    dataset_type,
+    adf_type
+)
+VALUES (
+    'CASSANDRA',
+    'String',
+    'string'
+);
+GO
